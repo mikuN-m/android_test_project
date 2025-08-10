9f94a48709268e8525456d7a3e3a28e58e86be3f
miku <nunon4msmrn@icloud.com>
2025-08-11 00:58:16 +0900

    名前を変えた

--- app/src/main/java/com/example/android_test_project/MainActivity.kt
+++ app/src/main/java/com/example/android_test_project/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Android_test_projectTheme {
-        Greeting("Android")
+        Greeting("miku")
     }
 }
